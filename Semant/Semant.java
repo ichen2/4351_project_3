97a1a70ae17fe5c0faaa9ee8036577e84983d7af
Ian Chen <ichen2@lsu.edu>
2021-03-23 21:05:09 -0500

    transExp for expressions

--- Semant/Semant.java
+++ Semant/Semant.java
@@ -74,6 +74,32 @@ public class Semant {
       result = transExp((Absyn.OpExp)e);
     else if (e instanceof Absyn.LetExp)
       result = transExp((Absyn.LetExp)e);
+    else if (e instanceof Absyn.VarExp)
+	    result = transExp((Absyn.VarExp)e);
+    else if (e instanceof Absyn.NilExp)
+	    result = transExp((Absyn.NilExp)e);
+    else if (e instanceof Absyn.IntExp)
+	    result = transExp((Absyn.IntExp)e);
+    else if (e instanceof Absyn.StringExp)
+	    result = transExp((Absyn.StringExp)e);
+    else if (e instanceof Absyn.CallExp)
+	    result = transExp((Absyn.CallExp)e);
+    else if (e instanceof Absyn.RecordExp)
+ 	    result = transExp((Absyn.RecordExp)e);
+    else if (e instanceof Absyn.SeqExp)
+	    result = transExp((Absyn.SeqExp)e);
+    else if (e instanceof Absyn.AssignExp)
+	    result = transExp((Absyn.AssignExp)e);
+    else if (e instanceof Absyn.IfExp)
+	    result = transExp((Absyn.IfExp)e);
+    else if (e instanceof Absyn.WhileExp)
+	    result = transExp((Absyn.WhileExp)e);
+    else if (e instanceof Absyn.ForExp)
+	    result = transExp((Absyn.ForExp)e);
+    else if (e instanceof Absyn.BreakExp)
+	    result = transExp((Absyn.BreakExp)e);
+    else if (e instanceof Absyn.ArrayExp)
+	    result = transExp((Absyn.ArrayExp)e);
     else throw new Error("Semant.transExp");
     e.type = result.ty;
     return result;
